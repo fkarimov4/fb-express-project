86b77cb21a51e0af50742aadb454bf9519966860
Farrukh Karimov <farrukhkarimov@Farrukhs-MacBook-Pro.local>
2022-02-04 15:45:22 -0500

    created express api with post, get and patch requests for customers endpoint

---
+++ src/customers-col.js
@@ -0,0 +1,30 @@
+const { connectDb } = require("./connect-db");
+
+const custRef = connectDb().collection("customers");
+
+function getCustomersCol() {
+  return custRef
+    .get()
+    .then((snapshot) => {
+      const customers = [];
+      snapshot.forEach((doc) => {
+        customers.push({ id: doc.id, ...doc.data() });
+      });
+      return customers;
+    })
+    .catch(console.error);
+}
+
+function addCustomer(customer) {
+  return custRef
+    .add(customer)
+    .catch(console.error);
+}
+
+function updateCustomer(customerId, dataToUpdate) {
+    return custRef
+    .doc(customerId)
+    .update(dataToUpdate)
+}
+
+module.exports = { getCustomersCol, addCustomer, updateCustomer };
---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const { getCustomersCol, addCustomer, updateCustomer } = require('./customers-col')
+
+const app = express()
+app.use(express.json())
+
+app.get('/customers', (req, res) => {
+    getCustomersCol()
+    .then(customers => res.send(customers))
+})
+
+app.post('/customers', (req, res) => {
+    addCustomer(req.body)
+    .then(() => res.send("Successfully added user!").status(200))
+})
+
+app.patch('/customers/:customerId', (req, res) => {
+    updateCustomer(req.params.customerId, req.body)
+    .then(() => res.send("Successfully updated user!").status(200))
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
